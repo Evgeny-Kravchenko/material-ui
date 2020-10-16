cc55142f9c15638d446c2964a6e56e194a2e7469
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-10-16 11:29:06 +0300

    feat: add button of adding a new task

--- src/components/days-list/days-list.jsx
+++ src/components/days-list/days-list.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import DayItem from '@components/day-item';
-import { Grid } from '@material-ui/core';
+import { Grid, Fab, makeStyles } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyle = makeStyles({
+  root: {
+    position: 'fixed',
+    bottom: '20px',
+    right: '20px',
+  },
+});
 
 const DaysList = () => {
+  const classes = useStyle();
   const today = new Date();
   const startDay = new Date(+today - 10 * 24 * 60 * 60 * 1000);
   const datesArray = new Array(20).fill(null).map((_, index) => {
@@ -19,6 +29,9 @@ const DaysList = () => {
   return (
     <Grid container direction={'column'} spacing={10}>
       {days}
+      <Fab className={classes.root} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
     </Grid>
   );
 };
